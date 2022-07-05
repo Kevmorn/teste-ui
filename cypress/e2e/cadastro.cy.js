783cd7a88741623e79bddb618de7478bf947ad96
Kevmorn <cristiano.duro97@gmail.com>
2022-07-05 10:11:21 -0300

    teste site do gmail com a biblioteca faker.js

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,11 +1,19 @@
 /// <reference types="cypress"/>
 
-context('QUERO ME CADASTRAR', () => {
-    beforeEach(() => {
-        cy.visit('https://www.kabum.com.br/login')
-    });
-    it('Conseguir fazer o cadastro', () => {
-        
-    });
+const faker = require('faker');
 
+beforeEach(() => {
+    cy.visit('https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp')
+});
+context('Funciolalidade Pré Cadastro', () => {
+    it.only('deve completar cadastro com sucesso', () => {
+        cy.visit('https://accounts.google.com/signup')
+        cy.get('#firstName').type(faker.name.firstName(6))
+        cy.get('#lastName').type(faker.name.lastName())
+        cy.get('#username').type(faker.name.firstName())
+        cy.get('#passwd').type(faker.name())
+        cy.get('#confirm-passwd').type(faker.name())
+        cy.get('.VfPpkd-muHVFf-bMcfAe').click()
+        cy.get('#accountDetailsNext > .VfPpkd-dgl2Hf-ppHlrf-sM5MNb > .VfPpkd-LgbsSe > .VfPpkd-vQzf8d').click()
+    });
 });
